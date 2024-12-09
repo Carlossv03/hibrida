cea8134ea5d2b63fab96ee7451e6fc14166cdea6
Carlossv03 <carasala@espol.edu.ec>
2024-12-09 01:30:23 -0500

    Guia 17

---
+++ src/credentials.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBWT2lsgcCHhSVf-jg5ws4UK8psrwPYcnw",
+    authDomain: "hibrida-d75e8.firebaseapp.com",
+    projectId: "hibrida-d75e8",
+    storageBucket: "hibrida-d75e8.firebasestorage.app",
+    messagingSenderId: "291450050824",
+    appId: "1:291450050824:web:d28e7c34eeef83b59074bd",
+    measurementId: "G-CV3PS3QQEB"
+  };
+  
